614d65de3868cd90b291e2daef2f898023c1f80d
Eri <erysy77@gmail.com>
2025-01-18 15:17:12 -0300

    Adicionando operador Ternário

--- app.js
+++ app.js
@@ -5,20 +5,35 @@ let Mensagem = 'Olá...Td bem?';
 alert(Mensagem);
 
 // Declaração das variáveis 
-let numeroSecreto = 29; 
-let valorDigitado = prompt('Tente acertar o número secreto escondido entre 1 a 30');
+let numeroSecreto = 50; 
+let valorDigitado 
+let tentativas = 1;
 
-
+while(valorDigitado != numeroSecreto){
+   valorDigitado = prompt('Tente acertar o número secreto escondido');
+    
 // Verificação para ver se o valor digitado pelo usuário 
-// é igual ao número secreto. 
-if ( valorDigitado == numeroSecreto){
-    console.log(`Nice, o número secreto era ${numeroSecreto}`);
-}else{
-    alert('Não era esse...Tente novamente!');
-}
+// é igual ao número secreto ou não. Caso não seja
+// O Usuário receberá dicas 
 
-// Caso contrário é utilizado 'else' que indica quando o valorDigitado 
-// é falso, ou seja, não é igual ao númeroSecreto
+   if ( valorDigitado == numeroSecreto){
+        break;
+   }else{
+        tentativas++;
+        if (numeroSecreto > valorDigitado){
+            alert(`O numéro secreto é maior que ${valorDigitado}`);
+        }else{
+            alert(`O número secreto é menor que ${valorDigitado}`);
+        }
+        
+}
+}
+// Operador Ternário 
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativas';
+
+alert(`Nice, o número secreto era ${numeroSecreto}\nTentativas: ${palavraTentativa}`);
+
+
 
 
 
